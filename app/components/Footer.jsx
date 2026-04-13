bb529d4e5b0a52a29e41db3ecce6d521b8e4bffb
usmughal90 <usmughal90@gmail.com>
2026-04-13 16:26:13 +0500

    update footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full bg-white dark:bg-gray-950 border-t border-white/10 py-6 px-6 sm:px-10 lg:px-20">
       <div className="max-w-7xl mx-auto text-center">
         <p className="text-sm sm:text-base md:text-md text-zinc-900 dark:text-white font-normal">
-          Copyrights © 2024 - 2026 <a href={`${process.env.NEXT_PUBLIC_API_DOMAIN ? process.env.NEXT_PUBLIC_API_DOMAIN : '/'}`}  className="text-zinc-900 dark:text-white underline">Quick Paste Manager</a>. Product by <a href="https://www.codematics.co/" target="_blank" className="text-zinc-900 dark:text-white underline">Codematics Services (Private) Limited</a>.
+          Copyrights © 2024 - 2026 <a href={`${process.env.NEXT_PUBLIC_API_DOMAIN ? process.env.NEXT_PUBLIC_API_DOMAIN : '/'}`}  className="text-zinc-900 dark:text-white underline">Quick Paste Manager</a>
         </p>
       </div>
     </footer>
